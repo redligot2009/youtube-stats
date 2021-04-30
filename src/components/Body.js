bc7a09b31db530b0e1f8fb64fbbac955709927a1
redligot2009 <redligot2009.forreal@gmail.com>
2021-04-30 23:16:53 +0800

    Added then catch block to figure out what's going on

--- src/components/Body.js
+++ src/components/Body.js
@@ -16,13 +16,19 @@ class Body extends Component {
         id: term
       }
     })
-    this.setState({
-      everything: response.data.items,
-      tags: response.data.items[0].snippet.tags,
-      title: response.data.items[0].snippet.title,
-      channelTitle: response.data.items[0].snippet.channelTitle,
-      views: response.data.items[0].statistics.viewCount
+    .then((response)=>{
+      this.setState({
+        everything: response.data.items,
+        tags: response.data.items[0].snippet.tags,
+        title: response.data.items[0].snippet.title,
+        channelTitle: response.data.items[0].snippet.channelTitle,
+        views: response.data.items[0].statistics.viewCount
+      })
     })
+    .catch((error)=>{
+      console.log(error);
+    })
+    
   }
   render() {
     return(
